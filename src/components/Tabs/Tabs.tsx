9d49e87abec47b0405a51f4b878fb6ff1b19648f
DDefty <dawid.kogut99@gmail.com>
2025-10-01 12:22:36 +0200

    Implement Tabs component and update App to manage selected tab state

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,47 @@
-export const Tabs = () => {};
+import React from 'react';
+
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type TabsProps = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onChange: (tabId: string) => void;
+};
+
+export const Tabs = ({ tabs, selectedTabId, onChange }: TabsProps) => {
+  const getTab = (tabId: string): Tab | undefined => {
+    return tabs.find(tab => tab.id === tabId);
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab, index) => (
+            <li
+              key={index}
+              className={tab.id === selectedTabId ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => onChange(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {getTab(selectedTabId)?.content}
+      </div>
+    </div>
+  );
+};
